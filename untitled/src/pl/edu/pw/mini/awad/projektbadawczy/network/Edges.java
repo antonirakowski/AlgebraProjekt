b531fc84914e39f2bb26eac17430fc382deebcc7
aryski <awr271@gmail.com>
2024-06-09 09:13:41 +0200

    xd

---
+++ untitled/src/pl/edu/pw/mini/awad/projektbadawczy/network/Edges.java
@@ -0,0 +1,28 @@
+package pl.edu.pw.mini.awad.projektbadawczy.network;
+
+import pl.edu.pw.mini.awad.projektbadawczy.Matrix;
+import pl.edu.pw.mini.awad.projektbadawczy.Vector;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Edges {
+    final Layer layer1;
+    final Layer layer2;
+    final Matrix weights;
+
+    public Edges(Layer layer1, Layer layer2) {
+        this.layer1 = layer1;
+        this.layer2 = layer2;
+        Random random = new Random();
+        ArrayList<Vector> weights = new ArrayList<Vector>();
+        for (int i = 0; i < layer1.neurons.size(); i++) {
+            ArrayList<Double> weightsFromLayer1ToLayer2 = new ArrayList<>();
+            for (int j = 0; j < layer2.neurons.size(); j++) {
+                weightsFromLayer1ToLayer2.add(random.nextDouble());
+            }
+            weights.add(new Vector(weightsFromLayer1ToLayer2));
+        }
+        this.weights = new Matrix(weights);
+    }
+}
--- untitled/src/pl/edu/pw/mini/awad/projektbadawczy/network/NeuralNetwork.java
+++ untitled/src/pl/edu/pw/mini/awad/projektbadawczy/network/NeuralNetwork.java
@@ -7,13 +7,19 @@ public class NeuralNetwork {
 
     private int numLayers;
     private ArrayList<Layer> layers;
+    private ArrayList<Edges> edges;
 
     NeuralNetwork (ArrayList<Integer> sizes) {
         numLayers = sizes.size();
         ArrayList<Layer> layers = new ArrayList<>();
-        for (int i = 1; i < sizes.size(); i++) {
+        for (int i = 0; i < sizes.size(); i++) {
             layers.add(new Layer(sizes.get(i)));
         }
         this.layers = layers;
+        ArrayList<Edges> edges = new ArrayList<>();
+        for (int i = 0; i < sizes.size()-1; i++) {
+            edges.add(new Edges(layers.get(i), layers.get(i + 1)));
+        }
+        this.edges = edges;
     }
 }
