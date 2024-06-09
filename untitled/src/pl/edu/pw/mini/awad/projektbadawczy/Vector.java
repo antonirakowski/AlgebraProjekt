b08b2513d30c7ad5a03be9444f4b4e2be63503c4
Antoni Rakowski <anthonyrakowski1@gmail.com>
2024-06-09 08:30:38 +0200

    Vectors added

--- untitled/src/pl/edu/pw/mini/awad/projektbadawczy/Vector.java
+++ untitled/src/pl/edu/pw/mini/awad/projektbadawczy/Vector.java
@@ -22,4 +22,35 @@ public class Vector {
         }
         return new Vector(result);
     }
+
+    public static Vector subtract(Vector v1, Vector v2) {
+        if (v1.length != v2.length) {
+            throw new IllegalArgumentException("Vectors must have the same length");
+        }
+        ArrayList<Double> result = new ArrayList<>();
+        for (int i = 0; i < v1.length; i++) {
+            result.add(v1.values.get(i) - v2.values.get(i));
+        }
+        return new Vector(result);
+    }
+
+    public static Double dot(Vector v1, Vector v2) {
+        if (v1.length != v2.length) {
+            throw new IllegalArgumentException("Vectors must have the same length");
+        }
+        ArrayList<Double> result = new ArrayList<>();
+        Double sum = 0.0;
+        for (int i = 0; i < v1.length; i++) {
+            sum += v1.values.get(i) * v2.values.get(i);
+        }
+        return sum;
+    }
+
+    public static Vector multiply(Double scalar, Vector v) {
+        ArrayList<Double> result = new ArrayList<>();
+        for (int i = 0; i < v.length; i++) {
+            result.add(v.values.get(i) * scalar);
+        }
+        return new Vector(result);
+    }
 }
