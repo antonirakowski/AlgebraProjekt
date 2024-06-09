ff7a298f4295e2a2370395a592bb7c6aac38e318
aryski <awr271@gmail.com>
2024-06-09 11:08:32 +0200

    xd

--- untitled/src/pl/edu/pw/mini/awad/projektbadawczy/network/Edges.java
+++ untitled/src/pl/edu/pw/mini/awad/projektbadawczy/network/Edges.java
@@ -11,7 +11,7 @@ public class Edges {
     final Layer layer2;
     final Matrix weights;
 
-    public Edges(Layer layer1, Layer layer2, boolean isRandom) {
+    public Edges(Layer layer1, Layer layer2) {
         this.layer1 = layer1;
         this.layer2 = layer2;
         Random random = new Random();
@@ -19,11 +19,7 @@ public class Edges {
         for (int i = 0; i < layer1.neurons.size(); i++) {
             ArrayList<Double> weightsFromLayer1ToLayer2 = new ArrayList<>();
             for (int j = 0; j < layer2.neurons.size(); j++) {
-                double value = 0;
-                if (isRandom) {
-                    value = random.nextDouble();
-                }
-                weightsFromLayer1ToLayer2.add(value);
+                weightsFromLayer1ToLayer2.add(random.nextDouble());
             }
             weights.add(new Vector(weightsFromLayer1ToLayer2));
         }
